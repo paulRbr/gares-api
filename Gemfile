ca3fd290cfb43f35f854b14befe5732e955a8c00
Paul B <paul@bonaud.fr>
2015-03-11 22:17:58 +0100

    Update to Grape gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'rack', '1.5.2'
-gem 'sinatra'
-gem 'sinatra-contrib'
+gem 'grape'
 gem 'gares'
--- app.rb
+++ app.rb
@@ -1,19 +1,50 @@
-require 'sinatra/base'
-require 'sinatra/json'
+require 'grape'
 require 'gares'
 
-class App < Sinatra::Base
-  helpers Sinatra::JSON
+class App < Grape::API
+  format :json
 
-  get '/gare/:slug' do
-    gare = Gares::Search.new(params[:slug]).gares.last
-    if gare
-      json gare: {
+  resource :gares do
+    desc "Return a list of gares matching the given blob."
+    params do
+      requires :slug, type: String, desc: "Blob to search for gares."
+    end
+    route_param :slug do
+      get do
+        gares = Gares::Search.new(params[:slug]).gares
+
+        gares.map{ |gare| serialize_gare(gare) }
+      end
+    end
+
+    desc "Return a specific gare from a gares search."
+    params do
+      requires :id, type: Integer, desc: "Position of the gare from search."
+      requires :slug, type: String, desc: "Blob to search for gares."
+    end
+    route_param :slug do
+      route_param :id do
+        get do
+          gares = Gares::Search.new(params[:slug]).gares
+          begin
+            serialize_gare(gares[params[:id]])
+          rescue
+            {message: "id must be > -#{gares.size} and < #{gares.size}"}
+          end
+        end
+      end
+    end
+  end
+
+  helpers do
+    def serialize_gare(gare)
+      gare.nil? ? nil : {
         name: gare.name,
+        lat: gare.lat,
+        long: gare.long,
+        services: gare.services,
         borne: gare.has_borne?
       }
-    else
-      json gare: nil
     end
   end
 end
